5eaf009c6bb4c079dae0891d8f23f0ca33b9af1a
Deyl <soulvisman@gmail.com>
2019-03-29 12:46:39 +0600

    Create Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, withStyles } from '@material-ui/core';
+import { green } from '@material-ui/core/colors';
+
+// Customize css
+const styles = theme => ({
+  appBar: {
+    backgroundColor: green[500]
+  },
+  typography: {
+    fontFamily: 'Carter One, cursive',
+    fontSize: '23px',
+    color: green[50],
+    margin: '0 auto',
+    textAlign: 'center'
+  }
+});
+
+function Header(props) {
+  const { appBar, typography } = props.classes;
+  return (
+    <AppBar position="static">
+      <Toolbar className={appBar}>
+        <Typography variant="h6" className={typography}>
+          ToDo App
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default withStyles(styles)(Header);
