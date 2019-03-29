29ec9e329f6c1f96dfbcb76b214673769766f129
Deyl <soulvisman@gmail.com>
2019-03-29 13:20:28 +0600

    Apply Grid && withStyles | Insert h5 Typography variant

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline, Grid, withStyles, Typography } from '@material-ui/core';
 import Header from './components/Header';
 
+const styles = theme => ({
+  grid: {
+    padding: '10px 20px 0 20px'
+  }
+});
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const { grid } = this.props.classes;
     return (
       <React.Fragment>
         <CssBaseline />
         <Header />
+        <Grid container direction="column" className={grid}>
+          <Typography variant="h5">List of Tasks</Typography>
+        </Grid>
       </React.Fragment>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
