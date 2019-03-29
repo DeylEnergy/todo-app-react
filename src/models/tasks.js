5f6918d4218d10fd82716f9803281793c82d5afe
Deyl <soulvisman@gmail.com>
2019-03-29 16:32:21 +0600

    Add Tasks model

---
+++ src/models/tasks.js
@@ -0,0 +1,49 @@
+const tasks = [
+  {
+    id: 1,
+    status: 0,
+    name: 'Do the dishes',
+    desc: 'Lorem ipsum dolor sit amet',
+    due: 'Mar 30',
+    importance: 0,
+    tags: [1, 2]
+  },
+  {
+    id: 2,
+    status: 1,
+    name: 'Clean up the kitchen',
+    desc: 'Lorem ipsum dolor sit amet',
+    due: 'Mar 30',
+    importance: 2,
+    tags: [0, 2]
+  },
+  {
+    id: 3,
+    status: 2,
+    name: 'Walk with the dog',
+    desc: 'Lorem ipsum dolor sit amet',
+    due: 'Mar 30',
+    importance: 0,
+    tags: [1]
+  },
+  {
+    id: 4,
+    status: 1,
+    name: 'Fix the window',
+    desc: 'Lorem ipsum dolor sit amet',
+    due: 'Mar 30',
+    importance: 2,
+    tags: [2]
+  },
+  {
+    id: 5,
+    status: 2,
+    name: 'Goof off with friends',
+    desc: 'Lorem ipsum dolor sit amet',
+    due: 'Mar 30',
+    importance: 0,
+    tags: [2, 3]
+  }
+];
+
+export default tasks;
