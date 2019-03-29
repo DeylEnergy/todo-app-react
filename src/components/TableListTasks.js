34cef0565c9b621097cc3a1f3ddaa4bd693b6e35
Deyl <soulvisman@gmail.com>
2019-03-29 18:59:24 +0600

    Style tags with Material Chip Component

--- src/components/TableListTasks.js
+++ src/components/TableListTasks.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { TableBody, TableRow, TableCell } from '@material-ui/core';
+import { TableBody, TableRow, TableCell, Chip } from '@material-ui/core';
 import StatusSelector from './StatusSelector';
+import tags from '../models/tags';
 
 function TableListTasks(props) {
   const { tasks } = props;
@@ -16,7 +17,16 @@ function TableListTasks(props) {
           <TableCell align="left">{task.desc}</TableCell>
           <TableCell align="left">{task.due}</TableCell>
           <TableCell align="left">{task.importance}</TableCell>
-          <TableCell align="left">{task.tags}</TableCell>
+          <TableCell align="left">
+            {task.tags.map(id => (
+              <Chip
+                label={tags[id]}
+                style={{
+                  marginLeft: '3px'
+                }}
+              />
+            ))}
+          </TableCell>
           <TableCell align="center">action buttons</TableCell>
         </TableRow>
       ))}
