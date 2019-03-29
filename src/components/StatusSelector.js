1a4520d3ac58232003934521dc5a1ce923e2bf0b
Deyl <soulvisman@gmail.com>
2019-03-29 18:38:06 +0600

    Create StatusSelector Component

---
+++ src/components/StatusSelector.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { IconButton, Menu, MenuItem } from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import status from '../models/status';
+
+const ITEM_HEIGHT = 48;
+
+class StatusSelector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      anchorEl: null,
+      currentStatus: props.currentStatus
+    };
+  }
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = newStatus => {
+    this.setState({ currentStatus: newStatus, anchorEl: null });
+  };
+
+  render() {
+    const { anchorEl, currentStatus } = this.state;
+    const open = Boolean(anchorEl);
+    return (
+      <span>
+        {status[currentStatus]}
+        <IconButton
+          aria-label="more"
+          aria-owns={open ? 'long-menu' : undefined}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="long-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={this.handleClose}
+          PaperProps={{
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: 200
+            }
+          }}
+        >
+          {status.map((option, id) => (
+            <MenuItem key={option} onClick={this.handleClose.bind(null, id)}>
+              {option}
+            </MenuItem>
+          ))}
+        </Menu>
+      </span>
+    );
+  }
+}
+
+export default StatusSelector;
