a549d1f2fc235fbcee2d262b08120433a475e1d5
Deyl <soulvisman@gmail.com>
2019-03-30 12:45:24 +0600

    Set top && bottom bars

--- src/components/ModifyTaskPanel.js
+++ src/components/ModifyTaskPanel.js
@@ -1,15 +1,48 @@
 import React from 'react';
-import { Drawer } from '@material-ui/core';
+import { Drawer, CssBaseline, AppBar, Toolbar, withStyles, Button } from '@material-ui/core';
+
+const styles = theme => ({
+  content: {
+    padding: '80px 10px 0 10px'
+  },
+  appBarTop: {
+    top: 0,
+    bottom: 'auto'
+  },
+  appBarBottom: {
+    top: 'auto',
+    bottom: 0
+  },
+  toolbar: {
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
 
 function ModifyTaskPanel(props) {
-  const { isOpen, toggleModifyPanel } = props;
+  const { isOpen, toggleModifyPanel, classes } = props;
   return (
     <div>
       <Drawer open={isOpen} onClose={toggleModifyPanel}>
-        content of drawer
+        <React.Fragment>
+          <CssBaseline />
+          <AppBar position="fixed" className={classes.appBarTop}>
+            <Toolbar className={classes.toolbar}>
+              <div>Add Task</div>
+            </Toolbar>
+          </AppBar>
+          <div className={classes.content}>Some content here</div>
+          <AppBar position="fixed" className={classes.appBarBottom}>
+            <Toolbar>
+              <Button>Save</Button>
+              <Button>Cancel</Button>
+            </Toolbar>
+          </AppBar>
+        </React.Fragment>
       </Drawer>
     </div>
   );
 }
 
-export default ModifyTaskPanel;
+export default withStyles(styles)(ModifyTaskPanel);
