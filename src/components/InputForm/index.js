eef1bff4f773b82d0c15cdd536d7f7b56d5a5f3f
Deyl <soulvisman@gmail.com>
2019-03-31 08:30:52 +0600

    Bind Formik to Form in InputForm/index.js

---
+++ src/components/InputForm/index.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Formik } from 'formik';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Paper from '@material-ui/core/Paper';
+import { object as yupObject, string as yupString } from 'yup';
+import Form from './form';
+
+const styles = theme => ({
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit * 5}px ${theme.spacing.unit * 5}px ${theme.spacing.unit * 5}px`,
+    container: {
+      maxWidth: '200px'
+    }
+  }
+});
+
+const validationSchema = yupObject({
+  name: yupString('Enter a name').required('Name is required')
+});
+
+class InputForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const classes = this.props;
+    const { mutation } = this.props;
+    // 'mutation' prop comes from App.js
+    const values = { name: '', desc: '', due: '', status: 0, importance: '' };
+    const handleSubmit = vals => {
+      mutation.onSubmit(vals); // it invokes descedent's method
+    };
+    return (
+      <React.Fragment>
+        <div className={classes.container}>
+          <Paper elevation={1} className={classes.paper} />
+          <Formik
+            render={props => <Form {...props} />}
+            validationSchema={validationSchema}
+            initialValues={values}
+            onSubmit={handleSubmit}
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default withStyles(styles)(InputForm);
